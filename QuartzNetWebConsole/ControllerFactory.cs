f6d2fe68b445034c4c78bac0bb124735ffd1a48e
mausch <mauricioscheffer@gmail.com>
2010-06-10 20:39:47 -0300

    job list in a group

---
+++ QuartzNetWebConsole/ControllerFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+
+namespace QuartzNetWebConsole {
+    public class ControllerFactory : IHttpHandlerFactory {
+        public IHttpHandler GetHandler(HttpContext context, string requestType, string url, string pathTranslated) {
+            var name = context.Request.RawUrl.Split('/').Last().Split('?')[0].Split('.')[0];
+            name = name.ToUpperInvariant().First() + new string(name.ToLowerInvariant().Skip(1).ToArray());
+            string typeName = string.Format("QuartzNetWebConsole.{0}Controller", name);
+            var type = Type.GetType(typeName, true, true);
+            if (type == null)
+                throw new Exception(string.Format("Type '{0}' not found", type));
+            return (IHttpHandler)Activator.CreateInstance(type);
+        }
+
+        public void ReleaseHandler(IHttpHandler handler) {}
+    }
+}
---
+++ QuartzNetWebConsole/JobGroupController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using MiniMVC;
+using Quartz;
+
+namespace QuartzNetWebConsole {
+    public class JobGroupController : Controller {
+        private readonly IScheduler scheduler = Setup.Scheduler();
+
+        public override IResult Execute(HttpContextBase context) {
+            var group = context.Request.QueryString["group"];
+            var jobNames = scheduler.GetJobNames(group);
+            var jobs = jobNames.Select(j => scheduler.GetJobDetail(j, group));
+            return new ViewResult(new {jobs, group}, ViewName);
+        }
+    }
+}
