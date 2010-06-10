a2e7730c16efa308063c5eaa4f134acd8cd87b85
mausch <mauricioscheffer@gmail.com>
2010-06-10 20:05:14 -0300

    started modeling stuff

---
+++ QuartzNetWebConsole/IndexController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+using MiniMVC;
+using Quartz;
+
+namespace QuartzNetWebConsole {
+    public class IndexController : Controller {
+        private readonly IScheduler scheduler = Setup.Scheduler();
+
+        public override IResult Execute(HttpContextBase context) {
+            return new ViewResult(new {
+                scheduler.SchedulerName,
+                scheduler.JobGroupNames,
+                scheduler.CalendarNames,
+                scheduler.InStandbyMode,
+                scheduler.IsShutdown,
+                scheduler.IsStarted,
+                scheduler.JobListenerNames,
+                scheduler.TriggerGroupNames,
+                scheduler.TriggerListenerNames,
+            }, ViewName);
+        }
+    }
+}
---
+++ QuartzNetWebConsole/Setup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Quartz;
+
+namespace QuartzNetWebConsole {
+    public static class Setup {
+        public static Func<IScheduler> Scheduler { get; set;}
+
+        static Setup() {
+            Scheduler = () => {
+                throw new Exception("Define QuartzNetWebConsole.Setup.Scheduler");
+            };
+        }
+    }
+}
