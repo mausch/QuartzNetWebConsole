141c57f31bd61507479298c44297382db1335daf
mausch <mauricioscheffer@gmail.com>
2010-06-11 00:42:03 -0300

    pause/resume job group in index

--- QuartzNetWebConsole/IndexController.cs
+++ QuartzNetWebConsole/IndexController.cs
@@ -9,22 +9,25 @@ namespace QuartzNetWebConsole {
         private readonly IScheduler scheduler = Setup.Scheduler();
 
         public override IResult Execute(HttpContextBase context) {
-            var pausedTriggerGroups = scheduler.GetPausedTriggerGroups();
             var triggerGroups = scheduler.TriggerGroupNames
-                .Select(t => new TriggerGroupWithStatus(t, pausedTriggerGroups.Contains(t)))
+                .Select(t => new GroupWithStatus(t, scheduler.IsTriggerGroupPaused(t)))
+                .ToArray();
+            var jobGroups = scheduler.JobGroupNames
+                .Select(j => new GroupWithStatus(j, scheduler.IsJobGroupPaused(j)))
                 .ToArray();
             return new ViewResult(new {
                 scheduler,
                 metadata = scheduler.GetMetaData(),
                 triggerGroups,
+                jobGroups,
             }, ViewName);
         }
 
-        public struct TriggerGroupWithStatus {
+        public struct GroupWithStatus {
             private readonly string name;
             private readonly bool paused;
 
-            public TriggerGroupWithStatus(string name, bool paused) {
+            public GroupWithStatus(string name, bool paused) {
                 this.name = name;
                 this.paused = paused;
             }
