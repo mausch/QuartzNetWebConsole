a2abcc9f275642998d505226f80cbd501480ff8f
mausch <mauricioscheffer@gmail.com>
2010-06-10 23:21:47 -0300

    trigger group page

---
+++ QuartzNetWebConsole/TriggerGroupController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using MiniMVC;
+using Quartz;
+
+namespace QuartzNetWebConsole {
+    public class TriggerGroupController : Controller {
+        private readonly IScheduler scheduler = Setup.Scheduler();
+
+        public override IResult Execute(HttpContextBase context) {
+            var group = context.Request.QueryString["group"];
+            var triggerNames = scheduler.GetTriggerNames(group);
+            var triggers = triggerNames
+                .Select(t => {
+                    var trigger = scheduler.GetTrigger(t, group);
+                    var state = scheduler.GetTriggerState(t, group);
+                    return new TriggerWithState(trigger, state);
+                });
+            var thisUrl = context.Request.RawUrl;
+            return new ViewResult(new {triggers, thisUrl, group}, ViewName);
+        }
+
+        public struct TriggerWithState {
+            private readonly Trigger trigger;
+            private readonly TriggerState state;
+
+            public TriggerWithState(Trigger trigger, TriggerState state) {
+                this.trigger = trigger;
+                this.state = state;
+            }
+
+            public Trigger Trigger {
+                get { return trigger; }
+            }
+
+            public TriggerState State {
+                get { return state; }
+            }
+        }
+    }
+}
