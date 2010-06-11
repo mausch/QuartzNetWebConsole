5b9f7bbdcdc1b7a186af275aeb01a06260097a35
mausch <mauricioscheffer@gmail.com>
2010-06-11 00:19:35 -0300

    show pause status of trigger group in index

--- QuartzNetWebConsole/IndexController.cs
+++ QuartzNetWebConsole/IndexController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Web;
 using MiniMVC;
 using Quartz;
@@ -8,10 +9,33 @@ namespace QuartzNetWebConsole {
         private readonly IScheduler scheduler = Setup.Scheduler();
 
         public override IResult Execute(HttpContextBase context) {
+            var pausedTriggerGroups = scheduler.GetPausedTriggerGroups();
+            var triggerGroups = scheduler.TriggerGroupNames
+                .Select(t => new TriggerGroupWithStatus(t, pausedTriggerGroups.Contains(t)))
+                .ToArray();
             return new ViewResult(new {
                 scheduler,
                 metadata = scheduler.GetMetaData(),
+                triggerGroups,
             }, ViewName);
+        }
+
+        public struct TriggerGroupWithStatus {
+            private readonly string name;
+            private readonly bool paused;
+
+            public TriggerGroupWithStatus(string name, bool paused) {
+                this.name = name;
+                this.paused = paused;
+            }
+
+            public string Name {
+                get { return name; }
+            }
+
+            public bool Paused {
+                get { return paused; }
+            }
         }
     }
 }
