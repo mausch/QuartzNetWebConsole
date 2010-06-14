76574c61a868fca033e54011b351477fdb1f11ae
mausch <mauricioscheffer@gmail.com>
2010-06-14 18:27:32 -0300

    triggers by job

---
+++ QuartzNetWebConsole/Controllers/TriggersByJobController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using MiniMVC;
+using Quartz;
+using QuartzNetWebConsole.Models;
+
+namespace QuartzNetWebConsole.Controllers {
+    public class TriggersByJobController : Controller {
+        private readonly IScheduler scheduler = Setup.Scheduler();
+
+        public override IResult Execute(HttpContextBase context) {
+            var group = context.Request.QueryString["group"];
+            var job = context.Request.QueryString["job"];
+            var thisUrl = context.Request.RawUrl;
+            var triggers = scheduler.GetTriggersOfJob(job, group)
+                .Select(t => {
+                    var state = scheduler.GetTriggerState(t.Name, t.Group);
+                    return new TriggerWithState(t, state);
+                });
+            return new ViewResult(new {triggers, thisUrl, group, job}, ViewName);
+        }
+    }
+}
