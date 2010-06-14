696459f8fe75b96f76a4f4c600d5cd3a875dc603
mausch <mauricioscheffer@gmail.com>
2010-06-14 00:54:22 -0300

    trigger and job listeners, including global ones

--- QuartzNetWebConsole/IndexController.cs
+++ QuartzNetWebConsole/IndexController.cs
@@ -18,12 +18,33 @@ namespace QuartzNetWebConsole {
             var calendars = scheduler.CalendarNames
                 .Select(name => new {name, description = scheduler.GetCalendar(name).Description})
                 .ToArray();
+
+            var globalJobListeners = scheduler.GlobalJobListeners
+                .Cast<IJobListener>()
+                .Select(j => new {name = j.Name, type = j.GetType(), global = true});
+            var otherJobListeners = scheduler.JobListenerNames
+                .Cast<string>()
+                .Select(j => scheduler.GetJobListener(j))
+                .Select(j => new {name = j.Name, type = j.GetType(), global = false});
+            var jobListeners = globalJobListeners.Concat(otherJobListeners).ToArray();
+
+            var globalTriggerListeners = scheduler.GlobalTriggerListeners
+                .Cast<ITriggerListener>()
+                .Select(l => new {name = l.Name, type = l.GetType(), global = true});
+            var otherTriggerListeners = scheduler.TriggerListenerNames
+                .Cast<string>()
+                .Select(n => scheduler.GetTriggerListener(n))
+                .Select(l => new { name = l.Name, type = l.GetType(), global = false });
+            var triggerListeners = globalTriggerListeners.Concat(otherTriggerListeners).ToArray();
+
             return new ViewResult(new {
                 scheduler,
                 metadata = scheduler.GetMetaData(),
                 triggerGroups,
                 jobGroups,
                 calendars,
+                jobListeners,
+                triggerListeners,
             }, ViewName);
         }
 
