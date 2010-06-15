62c7498fca58ea95131e25fd283a1f23ccea3159
mausch <mauricioscheffer@gmail.com>
2010-06-15 17:07:43 -0300

    basic circular buffer

---
+++ QuartzNetWebConsole.Tests/CircularBufferTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using QuartzNetWebConsole.Utils;
+
+namespace QuartzNetWebConsole.Tests {
+    [TestFixture]
+    public class CircularBufferTests {
+        [Test]
+        public void tt() {
+            var b = new CircularBuffer<int>(5);
+            foreach (var i in Enumerable.Range(0, 1000))
+                b.Add(i);
+            var a = b.ToArray();
+            Assert.AreEqual(5, a.Length);
+            Assert.AreEqual(999, a[0]);
+            Assert.AreEqual(998, a[1]);
+            Assert.AreEqual(997, a[2]);
+            Assert.AreEqual(996, a[3]);
+            Assert.AreEqual(995, a[4]);
+        }
+    }
+}
---
+++ QuartzNetWebConsole/Utils/CircularBuffer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace QuartzNetWebConsole.Utils {
+    public class CircularBuffer<T> : IEnumerable<T> {
+        private LazyList<T> list = LazyList<T>.Empty;
+        private readonly int capacity;
+
+        public CircularBuffer(int capacity) {
+            this.capacity = capacity;
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public void Add(T e) {
+            list = new LazyList<T>(e, list.Take(capacity-1));
+        }
+
+        public IEnumerator<T> GetEnumerator() {
+            return list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+    }
+}
