8864c6238a1c6388591d4e46b32f9c6fd07273b2
mausch <mauricioscheffer@gmail.com>
2012-01-10 22:57:05 -0300

    blah

--- QuartzNetWebConsole.Views/EnumerableExtensions.vb
+++ QuartzNetWebConsole.Views/EnumerableExtensions.vb
@@ -1,14 +1,14 @@
 ﻿Imports System.Runtime.CompilerServices
 
 Public Module EnumerableExtensions
-    <Extension()> _
+    <Extension()>
     Public Function WhereOdd(Of T)(ByVal s As IEnumerable(Of T)) As IEnumerable(Of T)
         Return s.Select(Function(e, i) New With {.e = e, .i = i}).
             Where(Function(e) e.i Mod 2 <> 0).
             Select(Function(e) e.e)
     End Function
 
-    <Extension()> _
+    <Extension()>
     Public Function WhereEven(Of T)(ByVal s As IEnumerable(Of T)) As IEnumerable(Of T)
         Return s.Select(Function(e, i) New With {.e = e, .i = i}).
             Where(Function(e) e.i Mod 2 = 0).
