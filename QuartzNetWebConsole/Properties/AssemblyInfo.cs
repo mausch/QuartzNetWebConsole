a7f1dcadd5ae1227bf025012ce389981337738cc
mausch <mauricioscheffer@gmail.com>
2015-07-05 20:59:51 +0100

    Version 0.5.0

--- QuartzNetWebConsole/Properties/AssemblyInfo.cs
+++ QuartzNetWebConsole/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("QuartzNetWebConsole")]
-[assembly: AssemblyCopyright("Copyright © Mauricio Scheffer 2010-2013")]
+[assembly: AssemblyCopyright("Copyright © Mauricio Scheffer 2010-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
